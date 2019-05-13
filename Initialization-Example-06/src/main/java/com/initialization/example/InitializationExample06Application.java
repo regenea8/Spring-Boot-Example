caeb58b15edf32efcb97e218772a5570e8c5bff5
jh.lee <jh.lee@DESKTOP-5GA34SC>
2019-05-13 15:42:25 +0900

    Project initialization

---
+++ Initialization-Example-06/src/main/java/com/initialization/example/InitializationExample06Application.java
@@ -0,0 +1,13 @@
+package com.initialization.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializationExample06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitializationExample06Application.class, args);
+	}
+
+}
