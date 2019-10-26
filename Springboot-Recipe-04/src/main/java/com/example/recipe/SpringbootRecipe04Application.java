3c22b46cdb9c0bb02ba4c167e0a0ea2f8e7c19b5
REGENEA <REGENEA@DESKTOP-8PTE4MR>
2019-10-27 00:44:18 +0900

    Project initialization

---
+++ Springboot-Recipe-04/src/main/java/com/example/recipe/SpringbootRecipe04Application.java
@@ -0,0 +1,13 @@
+package com.example.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRecipe04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRecipe04Application.class, args);
+	}
+
+}
